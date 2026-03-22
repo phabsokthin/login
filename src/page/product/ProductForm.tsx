ce43be8923200e80642d483f6241c6a292c6b505
sok thin <phabsokthin992003@gmail.com>
2026-03-22 18:24:00 +0700

    testing add 123

--- src/page/product/ProductForm.tsx
+++ src/page/product/ProductForm.tsx
@@ -81,7 +81,7 @@ export default function ProductForm({
           style={{ backgroundColor: "snow", padding: "20px" }}
         >
           <div>
-            <label className="block text-sm font-medium">Title1</label>
+            <label className="block text-sm font-medium">Title1 234</label>
             <input
               type="text"
               name="title"
