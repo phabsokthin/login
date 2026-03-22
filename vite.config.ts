f2f5a8cbc52659d69f7ec24713e83a244459a86a
sok thin <phabsokthin992003@gmail.com>
2026-03-22 19:55:50 +0700

    update vite js

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-zustand-1/',
+  server: {
+    open: true,
+    port: 3001,
+  },
 })
